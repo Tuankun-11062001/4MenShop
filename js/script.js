53e1c270034d511cab6dd0f1ba28b4e6bf5d0f7e
Tuan <nhoxtuanhero1@gmail.com>
2022-04-16 11:44:05 +0700

    xong header

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+if(document.readyState === "loadding"){
+    console.log("has loaded")
+    document.addEventListener("DOMContentLoaded",ready);
+}else{
+    ready();
+}
+
+
+
+function ready(){
+// header sticky
+window.addEventListener("scroll",()=>{
+    const header = document.querySelector(".header");
+    const btnGoToTop = document.querySelector(".btn-gototop");
+    if(window.scrollY > 0){
+        header.classList.add("sticky")
+        btnGoToTop.classList.add("active");
+        btnGoToTop.addEventListener("click",goToTop)
+    }else{
+        header.classList.remove("sticky");
+        btnGoToTop.classList.remove("active");
+    }
+})
+
+
+function goToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// show and hide btn search
+
+const btnSearch = document.querySelector(".btn-search");
+const inputSearch = document.querySelector(".search");
+
+btnSearch.addEventListener('click',()=>{
+    inputSearch.classList.toggle("active")
+})
+
+// show and hide btn search
+
+const btnCart = document.querySelector(".btn-cart");
+const cardBox = document.querySelector(".cart__box");
+
+btnCart.addEventListener('click',()=>{
+    cardBox.classList.toggle("active")
+})
+}
+
+
