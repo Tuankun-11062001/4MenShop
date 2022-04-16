d4f578eac3d4656f6a90445ed09ec0254fe2b0ce
Tuan <nhoxtuanhero1@gmail.com>
2022-04-16 16:16:32 +0700

    hoan thanh breacrumb listProduct

--- js/script.js
+++ js/script.js
@@ -46,6 +46,25 @@ const cardBox = document.querySelector(".cart__box");
 btnCart.addEventListener('click',()=>{
     cardBox.classList.toggle("active")
 })
+
+// change image items when hover on it
+
+const item = document.querySelector(".contain__main__item")
+
+item.addEventListener("mouseover",changeImage)
+item.addEventListener("mouseout",changeImageOld)
+
+function changeImage(e){
+    const item = e.target.parentElement
+    let img = item.querySelector(".contain__main__item__photoLarge");
+    img.src = "https://4menshop.com/cache/image/300x400/images/thumbs/2022/04/quan-jeans-slimfit-theu-qj043-mau-xanh-16676.JPG"
+}
+function changeImageOld(e){
+    const item = e.target.parentElement
+    let img = item.querySelector(".contain__main__item__photoLarge");
+    img.src = "https://4menshop.com/cache/image/300x400/images/thumbs/2022/04/quan-jeans-slimfit-theu-qj043-mau-xanh_2_small-16676.JPG"
+}
+
 }
 
 
